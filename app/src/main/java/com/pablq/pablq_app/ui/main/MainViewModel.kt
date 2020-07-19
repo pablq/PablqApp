f2a20d3e617fb99d3043bf556914669b508f0b1f
Pablo Philipps <pabloignaciophilipps@gmail.com>
2020-07-19 17:46:13 -0500

    Begins building VM logic.

--- app/src/main/java/com/pablq/pablq_app/ui/main/MainViewModel.kt
+++ app/src/main/java/com/pablq/pablq_app/ui/main/MainViewModel.kt
@@ -1,7 +1,63 @@
 package com.pablq.pablq_app.ui.main
 
+import android.net.Uri
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.*
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val allSports = listOf(
+        Sport("mlb", "baseball"),
+        Sport("nhl", "puck"),
+        Sport("nfl", "football"),
+        Sport("nba", "basketball")
+    )
+
+    val selectedSport = MutableLiveData<Sport?>()
+
+    val games = MutableLiveData<List<Game>>()
+
+    val isLoading = MutableLiveData<Boolean>()
+
+    private val repository: IMainRespository = MainRepository()
+
+    fun loadGames() {
+        val league = selectedSport.value?.league
+        if (league != null) {
+            isLoading.value = true
+            games.value = repository.getGames(league) ?: listOf()
+            isLoading.value = false
+        }
+    }
+
+    fun wakeUp() {
+        repository.wakeUp()
+    }
+}
+
+interface IMainRespository {
+    fun wakeUp()
+    fun getGames(league: String): List<Game>?
+}
+
+class MainRepository: IMainRespository {
+    override fun wakeUp() {
+        // do nothing.
+    }
+
+    override fun getGames(league: String): List<Game>? {
+        return listOf(
+            Game("headline1", listOf("line1","line2"), "http://www.pablq.website"),
+            Game("headline2", listOf(), "http://www.pablq.website"),
+            Game("headline3", listOf("line1","line2", "line3"), "http://www.pablq.website")
+        )
+    }
+}
+
+data class Sport(val league: String, val imageName: String)
+
+data class Game(val headline: String, val lines: List<String>, val link: String) {
+    val description = lines.joinToString("\n")
+    val isFavorite = headline.toLowerCase(Locale.ROOT).contains("chicago")
+    val url = Uri.parse(link)
 }
